ff94f60e8bfcfff147eac7310a01284e19d71e67
manucasariego <manucasariego@gmail.com>
2017-06-05 14:57:14 +0200

    Initial push

---
+++ djangoApiTest/views.py
@@ -0,0 +1,26 @@
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+
+from util.giveAccommodations import get_accommodations
+
+
+@csrf_exempt
+def accommodations(request):
+    """
+    List all code snippets, or create a new snippet.
+    """
+
+    return JsonResponse(get_accommodations({'ip': get_client_ip(request)}))
+
+
+def get_client_ip(request):
+    x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
+    if x_forwarded_for:
+        ip = x_forwarded_for.split(',')[0]
+    else:
+        ip = request.META.get('REMOTE_ADDR')
+    return ip
+
+
+def get_client_ip(request):
+    return request.get_host()
---
+++ util/giveAccommodations.py
@@ -0,0 +1,15 @@
+def get_accommodations(filters={}):
+    response_data = {}
+    response_data['title'] = 'Title 1'
+    response_data['price'] = '1234'
+    response_data['link'] = 'www.google.es'
+    response_data['type'] = ['bed', 'bed2']
+    response_data = merge_2_dictionaries(response_data, filters)
+    # response_data['extra'] = filters.get('manu2', '')
+    return response_data
+
+
+def merge_2_dictionaries(dictionary1, dictionary2):
+    z = dictionary1.copy()
+    z.update(dictionary2)
+    return z
