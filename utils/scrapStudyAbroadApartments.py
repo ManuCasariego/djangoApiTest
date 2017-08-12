7b19ad2cf636a058d60f9e5681a0b3f0d583cd82
manucasariego <manucasariego@gmail.com>
2017-08-12 14:41:19 +0200

    SAA now only shows 201 apartments

--- utils/scrapStudyAbroadApartments.py
+++ utils/scrapStudyAbroadApartments.py
@@ -152,7 +152,7 @@ def studyAbroadApartmentsAccommodations(filters=None):
         if every_filter_is_passed:
             accommodations_to_return.append(accommodation)
 
-    accommodations_to_return = accommodations_to_return[0:200]
+    accommodations_to_return = accommodations_to_return[0:201]
     return accommodations_to_return
 
 #
