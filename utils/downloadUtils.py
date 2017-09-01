0e7785736dcdb2f8136bfee319eb3a387607dca8
manucasariego <manucasariego@gmail.com>
2017-09-01 11:27:59 +0200

    Corrected bugs

--- utils/downloadUtils.py
+++ utils/downloadUtils.py
@@ -3,7 +3,7 @@ import os
 
 
 def save_to_file(dir_obj, accommodations_array, accommodations_name):
-    print('se va a guardar el fichero ', accommodations_name)
+    print('se va a guardar el fichero ', accommodations_name, ' con ', len(accommodations_array), ' apartamentos.')
     filename = os.path.join(dir_obj, '../data/' + accommodations_name + '.json')
 
     file = open(filename, 'w+')
