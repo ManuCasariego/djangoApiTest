b7268c531335a6012db74cec3545ff7565f6789a
manucasariego <manucasariego@gmail.com>
2017-09-01 14:54:52 +0200

    Addded beRoomersLocal to the city list

--- utils/parametersUtils.py
+++ utils/parametersUtils.py
@@ -30,67 +30,67 @@ def adaptCities(filters):
     # Setafoot, uniplaces, beroomers, studyAbroadApartments, erasmusinn ...
 
     # USA
-    cities.append(['new-york', '', 'new-york-united-states', 'new-york', ''])
-    cities.append(['boston', '', 'boston-ma-united-states', '', ''])
+    cities.append(['new-york', '', 'new-york-united-states', 'new-york', '', 'new-york'])
+    cities.append(['boston', '', 'boston-ma-united-states', '', '', 'boston'])
 
     # Italy
-    cities.append(['rome', 'rome', 'rome-italy', 'rome', 'rome'])
-    cities.append(['florence', 'florence', 'florence-italy', 'florence', 'florence'])
-    cities.append(['bologna', 'bologna', 'bologna-italy', '', ''])
-    cities.append(['milan', 'milan', 'milan-italy', 'milan', 'milan'])
-    cities.append(['padua', 'padua', 'padova-italy', '', ''])
-    cities.append(['turin', 'turin', 'turin-italy', '', ''])
+    cities.append(['rome', 'rome', 'rome-italy', 'rome', 'rome', 'rome'])
+    cities.append(['florence', 'florence', 'florence-italy', 'florence', 'florence', 'florence'])
+    cities.append(['bologna', 'bologna', 'bologna-italy', '', '', 'bologna'])
+    cities.append(['milan', 'milan', 'milan-italy', 'milan', 'milan', 'milan'])
+    cities.append(['padua', 'padua', 'padova-italy', '', '', 'padua'])
+    cities.append(['turin', 'turin', 'turin-italy', '', '', 'turin'])
 
     # United Kindgom
-    cities.append(['london', 'london', 'london-united-kingdom', 'london', 'london'])
-    cities.append(['manchester', 'manchester', '', '', ''])
-    cities.append(['leeds', 'leeds', 'leeds-united-kingdom', '', ''])
-    cities.append(['cardiff', 'cardiff', 'cardiff-united-kingdom', '', ''])
+    cities.append(['london', 'london', 'london-united-kingdom', 'london', 'london', 'london'])
+    cities.append(['manchester', 'manchester', '', '', '', ''])
+    cities.append(['leeds', 'leeds', 'leeds-united-kingdom', '', '', 'leeds'])
+    cities.append(['cardiff', 'cardiff', 'cardiff-united-kingdom', '', '', 'cardiff'])
 
     # Portugal
-    cities.append(['porto', 'porto', 'porto-portugal', '', ''])
-    cities.append(['lisbon', 'lisbon', 'lisboa-portugal', '', ''])
-    cities.append(['coimbra', 'coimbra', '', '', ''])
+    cities.append(['porto', 'porto', 'porto-portugal', '', '', 'porto'])
+    cities.append(['lisbon', 'lisbon', 'lisboa-portugal', '', '', 'lisbon'])
+    cities.append(['coimbra', 'coimbra', '', '', '', ''])
 
     # Spain
-    cities.append(['madrid', 'madrid', 'madrid-spain', 'madrid', 'madrid'])
-    cities.append(['barcelona', 'barcelona', 'barcelona-spain', 'barcelona', 'barcelona'])
-    cities.append(['valencia', 'valencia', 'valencia-spain', 'valencia', ''])
-    cities.append(['granada', 'granada', 'granada-spain', 'granada', 'granada'])
-    cities.append(['seville', 'seville', 'seville-spain', '', ''])
-    cities.append(['salamanca', 'salamanca', 'salamanca-spain', '', ''])
-    cities.append(['palma-de-mallorca', 'palma-de-mallorca', '', '', ''])
-    cities.append(['alicante', 'alicante', 'alicante-spain', '', ''])
-    cities.append(['malaga', 'malaga', 'malaga-spain', '', ''])
-    cities.append(['zaragoza', 'zaragoza', 'zaragoza-spain', '', 'zaragoza'])
-    cities.append(['cordova', 'cordova', 'cordoba-spain', '', ''])
-    cities.append(['murcia', 'murcia', 'murcia-spain', '', ''])
-    cities.append(['cadiz', 'cadiz', 'cadiz-spain', '', ''])
-    cities.append(['san-sebastian', '', '', 'san-sebastian', ''])
+    cities.append(['madrid', 'madrid', 'madrid-spain', 'madrid', 'madrid', 'madrid'])
+    cities.append(['barcelona', 'barcelona', 'barcelona-spain', 'barcelona', 'barcelona', 'barcelona'])
+    cities.append(['valencia', 'valencia', 'valencia-spain', 'valencia', '', 'valencia'])
+    cities.append(['granada', 'granada', 'granada-spain', 'granada', 'granada', 'granada'])
+    cities.append(['seville', 'seville', 'seville-spain', '', '', 'seville'])
+    cities.append(['salamanca', 'salamanca', 'salamanca-spain', '', '', 'salamanca'])
+    cities.append(['palma-de-mallorca', 'palma-de-mallorca', '', '', '', ''])
+    cities.append(['alicante', 'alicante', 'alicante-spain', '', '', 'alicante'])
+    cities.append(['malaga', 'malaga', 'malaga-spain', '', '', 'malaga'])
+    cities.append(['zaragoza', 'zaragoza', 'zaragoza-spain', '', 'zaragoza', 'zaragoza'])
+    cities.append(['cordova', 'cordova', 'cordoba-spain', '', '', 'cordova'])
+    cities.append(['murcia', 'murcia', 'murcia-spain', '', '', 'murcia'])
+    cities.append(['cadiz', 'cadiz', 'cadiz-spain', '', '', 'cadiz'])
+    cities.append(['san-sebastian', '', '', 'san-sebastian', '', ''])
 
     # France
-    cities.append(['paris', 'paris', 'paris-france', 'paris', ''])
+    cities.append(['paris', 'paris', 'paris-france', 'paris', '', 'paris'])
 
     # Germany
-    cities.append(['berlin', 'berlin', 'berlin-germany', '', 'berlin'])
-    cities.append(['munich', 'munich', '', '', ''])
+    cities.append(['berlin', 'berlin', 'berlin-germany', '', 'berlin', 'berlin'])
+    cities.append(['munich', 'munich', '', '', '', ''])
 
     # Poland
-    cities.append(['warsaw', '', '', '', 'warsaw'])
-    cities.append(['poznan', '', '', '', 'poznan'])
-    cities.append(['lodz', '', '', '', 'lodz'])
+    cities.append(['warsaw', '', '', '', 'warsaw', ''])
+    cities.append(['poznan', '', '', '', 'poznan', ''])
+    cities.append(['lodz', '', '', '', 'lodz', ''])
 
     # Turkey
-    cities.append(['istanbul', '', '', '', 'istanbul'])
+    cities.append(['istanbul', '', '', '', 'istanbul', ''])
 
     # Check Republic
-    cities.append(['prague', '', '', 'prague', ''])
+    cities.append(['prague', '', '', 'prague', '', ''])
 
-    cityString = filters.get('city', '')
+    city_string = filters.get('city', '')
 
-    if cityString != '':
+    if city_string != '':
         for city in cities:
-            if city[0] == cityString:
+            if city[0] == city_string:
                 return city
 
-    return ['madrid', 'madrid', 'madrid-spain', 'madrid', 'madrid']
+    return ['madrid', 'madrid', 'madrid-spain', 'madrid', 'madrid', 'madrid']
